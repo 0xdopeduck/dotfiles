f577dad1fedf1b5c7cea6065dbb0b0b120d342ef
0xdopeduck <0xdopeduck@example.com>
2024-02-28 23:28:09 +0530

    Updated download link

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -3,8 +3,9 @@
 ## Installing Fonts
 
 mkdir $HOME/.local/share/fonts/
+rm -rf $HOME/.local/share/fonts/*
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip -O $HOME/.local/share/fonts/JetBrainsMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip -O $HOME/.local/share/fonts/JetBrainsMono.zip
 
 unzip $HOME/.local/share/fonts/JetBrainsMono.zip -d $HOME/.local/share/fonts/
 
