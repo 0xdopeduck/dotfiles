9b0513cab589bc2e65c050028eebbcbf18fdb917
0xdopeduck <0xdopeduck@example.com>
2024-03-08 16:13:53 +0530

    Removed zellij for the moment

--- setup/zellij.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -LO https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
-rm -rf ~/.local/bin/zellij
-sudo tar -C ~/.local/bin -xzf zellij-x86_64-unknown-linux-musl.tar.gz
-rm -rf zellij-x86_64-unknown-linux-musl.tar.gz 
