58f9103c9973c7c9b0881dd2ab8a4bca29ccd5bf
0xdopeduck <0xdopeduck@example.com>
2026-01-20 19:25:22 +0530

    Added Fira Code

--- setup/fonts.sh
+++ setup/fonts.sh
@@ -5,10 +5,18 @@
 mkdir -p $HOME/.local/share/fonts/
 rm -rf $HOME/.local/share/fonts/*
 
+#JetBrainsMono
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip -O $HOME/.local/share/fonts/JetBrainsMono.zip
 
 unzip $HOME/.local/share/fonts/JetBrainsMono.zip -d $HOME/.local/share/fonts/
 
 rm $HOME/.local/share/fonts/JetBrainsMono.zip
+
+#FiraCode
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip -O $HOME/.local/share/fonts/FiraCode.zip
+
+unzip $HOME/.local/share/fonts/FiraCode.zip -d $HOME/.local/share/fonts/
+
+rm $HOME/.local/share/fonts/FiraCode.zip
 
 fc-cache -fv
