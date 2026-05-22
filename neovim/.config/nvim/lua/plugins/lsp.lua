45849b74e5c148e04971e911bffaea8a6a093aaa
0xdopeduck <33199852+0xdopeduck@users.noreply.github.com>
2026-05-23 01:14:15 +0530

    Added lua-lsp

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -2,11 +2,73 @@ vim.pack.add {
     "https://github.com/neovim/nvim-lspconfig"
 }
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = {
-    "go",
-  },
-  callback = function(args)
-    pcall(vim.lsp.enable("gopls"), args.buf)
-  end,
+-- Golang LSP
+vim.lsp.config("gopls", {
+    settings = {
+        gopls = {
+            staticcheck = true,
+            gofumpt = true,
+        }
+    }
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+        "go",
+    },
+    callback = function(args)
+        pcall(vim.lsp.enable("gopls"), args.buf)
+    end,
+})
+
+-- Lua LSP
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {
+                    "vim"
+                }
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file(
+                    "",
+                    true
+                ),
+                checkThirdParty = false,
+            },
+            completion = {
+                callSnippet = "Replace",
+            },
+            format = {
+                enable = true,
+            },
+        }
+    }
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+        "lua",
+    },
+    callback = function(args)
+        pcall(vim.lsp.enable("lua_ls"), args.buf)
+    end,
+})
+
+-- Autocomplete with lsp
+vim.api.nvim_create_autocmd(
+    "LspAttach",
+    {
+        callback = function(args)
+            vim.lsp.completion.enable(
+                true,
+                args.data.client_id,
+                args.buf,
+                {
+                    autotrigger = true,
+                }
+            )
+        end
+    }
+)
